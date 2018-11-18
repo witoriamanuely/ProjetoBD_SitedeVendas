697f4094a81b4657cda5cdecdd23fc849cf4c688
Anderson <anderson.nobrega62@gmail.com>
2018-11-18 00:54:43 -0300

    Excluindo arquivos não necessarios e modificando a tela de produtos do site

--- gulpfile.js
+++ gulpfile.js
@@ -28,16 +28,4 @@ gulp.task('serve', ['sass'], function() {
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-// Move Fonts to src/fonts
-gulp.task('fonts', function() {
-  return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('src/fonts'))
-})
-
-// Move Font Awesome CSS to src/css
-gulp.task('fa', function() {
-  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest('src/css'))
-})
-
-gulp.task('default', ['js','serve', 'fa', 'fonts']);
+gulp.task('default', ['js','serve']);
