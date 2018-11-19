384ef465695cc6385a69ca0088070f14c65d651f
witoriamanuely <manuelycavalcante@gmail.com>
2018-11-19 01:11:33 -0300

    Adicionando user.js que possui a função de adionar na tabela de usuario, pegar todos, atualizar, e deletar, adicionando função no server.js que conecta com o banco, porem falta testar a conexão

--- src/server.js
+++ src/server.js
@@ -1,7 +1,29 @@
-const express = require('express')
-const app = express()
-const bodyParser = require ('body-parser'); 
 
+
+
+const express = require('express');
+const app = express();
+const bodyParser = require ('body-parser'); 
+const pg = require('pg');
+const connectionString = 'postgres://bluebird:bluebird@localhost/bluebird';
+ 
+app.get('/', function (req, res, next) {
+    pg.connect(connectionString,function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+       client.query('SELECT * FROM get', [1],function(err,result) {
+           done(); // closing the connection;
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           res.status(200).send(result.rows);
+       });
+    });
+});
+ 
 
 app.set ('view engine', 'ejs')
 app.use (express.static('public'));
@@ -24,23 +46,19 @@ app.get('/produtos', function (req, res) {
   res.render('produtos')
 })
 
-app.post('/cadastro', function (req, res) {
-  /*let primeiroNome = req.body.primeiroNome;
-  let sobrenome = req.body.sobrenome;
-  let email = req.body.email;
-  let bairro = req.body.bairro;
-  let cep = req.body.cep;
-  let tel1 = req.body.telefone1;
-  let tel2 = req.body.telefone2;
-  let password = req.body.password;
-  res.render('cadastro');*/
-})
 
 app.post('/login', function (req, res) {
-  console.log(req.body.email);
-  console.log(req.body.password);
   res.render('login');
 })
+
+app.post('/cadastro', function(req, res){
+res.render('cadastro');
+}
+  
+)
+
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
